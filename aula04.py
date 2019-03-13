7b24418e29ed14dc031ea087806f81fb2f4d653a
Aluno <rafael.zottesso@ifpr.edu.br>
2019-03-13 20:05:27 -0300

    Uso do print com o format.

--- aula04.py
+++ aula04.py
@@ -20,3 +20,17 @@ if(x>=y):
 else:
     # Se a condição anterior for falsa, este conteúdo será executado
     print("O valor de Y é maior!")
+
+
+"""
+Uso do print com a função "".format()
+
+"""
+
+nome = "Rafael"
+email = "rafael.zottesso@ifpr.edu.br"
+idade = 31
+
+print("Olá", nome, ". Seu email é", email," e sua idade é", idade, ".")
+
+print("Olá {}. Seu email é {} e sua idade é {}.".format(nome, email, idade ) )
---
+++ teste.py
@@ -0,0 +1,7 @@
+nome = "Rafael"
+email = "rafael.zottesso@ifpr.edu.br"
+idade = 31
+
+print("Olá", nome, ". Seu email é", email," e sua idade é", idade, ".")
+
+print("Olá {}. Seu email é {} e sua idade é {}.".format(nome, email, idade ) )
