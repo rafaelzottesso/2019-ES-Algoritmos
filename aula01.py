4ce39b26d5573caf115773831fa132aa0a5b546e
Aluno <rafael.zottesso@ifpr.edu.br>
2019-03-13 19:38:50 -0300

    Comandos de entrada e saída.

---
+++ aula01.py
@@ -0,0 +1,31 @@
+# print exibe uma mensagem na tela
+print("============================")
+
+# \n serve para quebrar uma linha
+# print("Bem vindos \n\n\n ao IFPR!")
+
+# \ - serve para anular o próximo caractere
+# print(" \\ ")
+
+# Assim, obrigamos o print a imprimir a "
+# print("Aula 01 de \"algoritmos\" :D")
+
+# O comando input é usado para entrada de dados
+# Ele "pega" o que foi digitado pelo usuário e armazena na variável "nome"
+nome = input("Digite seu nome: ")
+
+# A vírgula separa as coisas que o print vai mostar na tela
+print("Bem vindo ao sistema,", nome)
+
+# Você pode ter várias coisas, como: variáveis, strings (mensagens), números, etc
+print("um ", "dois ", "três ", nome, " quatro")
+print("Olá ", nome, ". Bem vindo!")
+print("============================")
+
+
+# Lista de Comandos:
+
+# cls - limpa a tela
+# dir - exibe uma lista de arquivos e diretórios
+# cd pasta - abre a pasta no terminal
+# python arquivo.py - executa o arquivo no terminal
