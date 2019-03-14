977323134ed7c4f6f3a34138de46f6875d73b14e
Aluno <rafael.zottesso@ifpr.edu.br>
2019-03-13 22:41:02 -0300

    Resolução do exercício 1.

---
+++ aula04_exercicios.py
@@ -0,0 +1,8 @@
+# Faça um Programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+num = input("Digite um número: ")
+num = int(num)
+
+if(num > 0):
+    print("Positivo!")
+else:
+    print("Negativo!")
