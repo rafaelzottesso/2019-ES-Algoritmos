6c69f3106e4458ad9feb542737dd38d90ea78062
Aluno <rafael.zottesso@ifpr.edu.br>
2019-04-17 22:22:17 -0300

    IntroduÃ§Ã£o ao laÃ§o de repetiÃ§Ã£o com while.[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[

---
+++ aula11-while.py
@@ -0,0 +1,29 @@
+"""
+-- Estrutura básica do laço de repetição while --
+
+variável de controle iniciada
+while(condição):
+    .
+    .
+    .
+    incremento da variável de controle
+
+"""
+
+# Variável de controle iniciada
+cont = 1
+# Condição do while montada de acordo com a variável de controle
+while( cont <= 1000 ):
+    # Código a ser processado
+    print("Valor de cont:", cont)
+
+    # Dentro do while pode ter qualquer coisa...
+    # Pedir coisa para usuário, if, elif, else, etc
+    # inclusive outro while :ooo
+
+    # Alteração/incremento no valor da variável de contrle
+    cont = cont + 1
+    # Fim do while
+
+# Continuação do programa, independente do resultado do while
+print("Fora do while!")
