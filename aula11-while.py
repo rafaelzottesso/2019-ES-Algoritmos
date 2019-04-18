d5ba7c139b1436b96b3a5088113c9c09c051fdd7
Aluno <rafael.zottesso@ifpr.edu.br>
2019-04-17 22:41:37 -0300

    Exemplos com while.

--- aula11-while.py
+++ aula11-while.py
@@ -10,21 +10,36 @@ while(condição):
 
 """
 
-# Variável de controle iniciada
-cont = 1
-# Condição do while montada de acordo com a variável de controle
-while( cont <= 1000 ):
-    # Código a ser processado
-    print("Valor de cont:", cont)
+# # Variável de controle iniciada
+# cont = 1
+# # Condição do while montada de acordo com a variável de controle
+# while( cont <= 1000 ):
+#     # Código a ser processado
+#     print("Valor de cont:", cont)
+#
+#     # Dentro do while pode ter qualquer coisa...
+#     # Pedir coisa para usuário, if, elif, else, etc
+#     # inclusive outro while :ooo
+#
+#     # Alteração/incremento no valor da variável de contrle
+#     cont = cont + 1
+#     # Fim do while
 
-    # Dentro do while pode ter qualquer coisa...
-    # Pedir coisa para usuário, if, elif, else, etc
-    # inclusive outro while :ooo
+# Fazendo o contador inverso...
+cont = 5
+while (cont >= 1):
+    print(cont)
+    cont = cont - 1
 
-    # Alteração/incremento no valor da variável de contrle
-    cont = cont + 1
-    # Fim do while
-    
+# Pedindo várias notas ao usuário
+cont = 1 # inicia o contador
+soma = 0 # inicia a variável de soma
+while(cont <= 4):
+    nota = int(input("Informe uma nota:"))
+    soma = soma + nota # junta a soma atual com a nota
+    cont = cont + 1 # aumenta o contador
+# Por fim, faz esse print 1x só porque está fora do while
+print("Média: ", soma/4)
 
 # Continuação do programa, independente do resultado do while
 print("Fora do while!")
