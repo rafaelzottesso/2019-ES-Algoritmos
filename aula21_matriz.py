189dc22857c09d2e511237b714d1965a54e1663a
Aluno <rafael.zottesso@ifpr.edu.br>
2019-06-26 22:06:38 -0300

    Criação dinâmica de matriz e seu preenchimento.

--- aula21_matriz.py
+++ aula21_matriz.py
@@ -15,3 +15,34 @@ for lin in range( 0, len(vet) ):
 
         # Imprimir o valor na linha "i" e coluna "j"
         print( vet[lin][col] )
+
+
+#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#*#
+
+# Criando uma matriz com tamanho customizado
+linhas = 3  # define a quantidade de linhas
+colunas = 3 # define a quantidade de colunas
+matriz = [] # cria um vetor vazio
+
+# Para cada linha da matriz que queremos criar...
+for i in range(0, linhas):
+    # Adiciona a quantidade de colunas informada
+    # m.append() adicona alguma coisa no final de "m"
+    matriz.append( [0] * colunas )
+
+# Plus!!
+# Documentação com mais funções prontas para trabalhar com listas/vetores
+# https://docs.python.org/3/tutorial/datastructures.html
+
+# Para cada linha...
+for linha in range(0, len(matriz)):
+    # Para cada coluna dentro de cada linha
+    for coluna in range(0, len(matriz[linha])):
+        # Armazena um número na matriz de acordo com a "linha" e "coluna" definida no for
+        valor = int(input("Informe valor para a linha {} e coluna {}: ".format(linha, coluna)))
+        # O input pode ser feito direto na matriz, aqui ficou separado por causa do tamanho da projeção
+        matriz[linha][coluna] = valor
+        # x = int(input("Informe valor para a linha {} e coluna {}:".format(linha, coluna)))
+
+print(matriz)
+
