a46c48f19f11102eaafb2955ce7637ac41441fa2
Rafael Zottesso <39135214+rafaelzottesso@users.noreply.github.com>
2019-07-31 22:20:14 -0300

    Vetor para tarefa de ordenação

---
+++ aula25_vetor_tarefa.py
@@ -0,0 +1,7 @@
+vetor = [15, 90, 70, 60, 80, 100, 0, 10]
+
+"""
+## Tarefa master ##
+Resolver no papel a ordenação deste vetor passo a passo
+utilizando a ideia dos três algoritmos de ordenação.
+"""
