66aee344225b5fa5db5291f5574a0b16bfbc66a6
Aluno <rafael.zottesso@ifpr.edu.br>
2019-10-02 22:01:22 -0300

    Manipulação de arquivos e conteúdo.

---
+++ aula32_arquivos.py
@@ -0,0 +1,83 @@
+"""
+ARQUIVOS: conjunto de dados armazenados em disco.
+
+Ações em arquivos:
+- Abrir arquivo   
+    open("nomearquivo.txt", modoAbertura)
+        modos: "r" - read (apenas lê dados do arquivo)
+               "w" - write (sempre "cria" um arquivo novo. Se ja existir, ele é "zerado")
+               "a" - append (escreve no final de arquivo ja existente)
+- Fechar arquivo
+    varArquivo.close()
+- Gravar dados no arquivo
+    varArquivo.write("conteúdo")
+- Ler os dados do arquivo
+     
+"""
+
+
+"""
+Criar um procedimento que receba
+como parâmetro o nome de um arquivo.
+O procedimento deverá:
+- abrir o arquivo para atualização 
+  (append), 
+- solicitar um novo valor ao usuário 
+  e salvar este nome no arquivo, 
+- salvar uma quebra de linha,
+- fechar o arquivo aberto.
+"""
+def escrever_nome(nome_arquivo):
+    xuxu = open(nome_arquivo,"a")
+    nome = input("Digite um nome para salvar:")
+    xuxu.write(nome + "\n")
+    xuxu.close()
+
+"""
+Crie um procedimento que receba
+o nome de um arquivo como parâmetro e
+abra o arquivo em modo leitura.
+Leia o arquivo
+Feche o arquivo
+"""
+def ler_arquivo(nome_arquivo):
+    palmeirasNaoTemMundial = open(nome_arquivo, "r", encoding="utf-8")
+    
+    conteudo = palmeirasNaoTemMundial.readlines()
+    
+    palmeirasNaoTemMundial.close()
+
+    for linha in range(0, len(conteudo)):
+        print( conteudo[linha] , end="")
+
+
+"""
+Ayslan Trevizan Possebom|ayslan@ifpr.edu.br|52.000,00
+
+
+Usamos a função split() de uma String para "quebrar" uma String
+A String será transformada em um vetor.
+Fornecemos o caractere usado para quebrar a String.
+Exemplo:
+
+dados = conteudo[linha].split("|")
+
+dados[0] = "Ayslan Trevizan Possebom"
+dados[1] = "ayslan@ifpr.edu.br"
+dados[2] = "52.000,00"
+"""
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+open("arquivo.txt", "r", encoding="utf-8")
+"""
---
+++ aula32_principal.py
@@ -0,0 +1,6 @@
+import aula32_arquivos
+
+#aula32_arquivos.escrever_nome("aula32_nomes.txt")
+
+aula32_arquivos.ler_arquivo("aula32_nomes.txt")
+
